d01d5d799d9cd6574c63abd162d1dc8bb89670c5
IttuAlex <ittu.alexandru93@gmail.com>
2025-10-22 12:18:46 +0300

    update backend: am facut setarile initiale pentru api

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,24 @@
-print("test")
+import os, jwt, datetime
+from flask import Flask, jsonify, request, session
+from pymongo import MongoClient, ASCENDING
+from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from bson import ObjectId
+
+app=Flask(__name__)
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "admin")
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+client = MongoClient(os.environ.get("MONGODB_URI", "mongodb://localhost:27017"))
+db = client[os.environ.get("MONGO_DB", "testdb")]
+users = db.users
+users.create_index([("username", ASCENDING)], unique=True)
+
+#de aici punem functiile pentru backend, tudor nu modifici ce e mai sus si mai jos de acest comentariu, mai poti adauga librarii
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
