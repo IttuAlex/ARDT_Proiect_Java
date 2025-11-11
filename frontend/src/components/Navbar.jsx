5387c9657a73365ef4a89a73fb2b0aa446e28f7e
DanaL08 <daniela.liciu@student.unitbv.ro>
2025-11-11 17:04:10 +0200

    Am schimbat rozul :))

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../images/LOGO2.png";
+import logo from "../images/LOGO3.png";
 
 
 const Navbar = () => {
